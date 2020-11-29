7fc971bbfaac35b1b2af0c0f868bac51191b7a62
19201080 <etienne.j.n@gmail.com>
2020-11-29 18:51:01 +0100

    Add requests and beautifulsoup to requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'pdfminer.six']
+requirements = ['Click>=7.0', 'pdfminer.six', 'requests', 'beautifulsoup4', ]
 
 setup_requirements = ['pytest-runner', ]
 
