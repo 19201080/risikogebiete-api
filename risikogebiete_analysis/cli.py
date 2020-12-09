7b710e9479a135e8143ca3c6868006b3497d810e
19201080 <etienne.j.n@gmail.com>
2020-12-09 18:49:15 +0100

    Add batch download and analysis to cli entry point

--- risikogebiete_analysis/cli.py
+++ risikogebiete_analysis/cli.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
+import csv
+from datetime import datetime
+import os
+import re
 import sys
 
-from risikogebiete_analysis.pdf_analysis.pdf_extractor import simple_extract
+import aiofiles
+
+from risikogebiete_analysis.pdf_analysis.pdf_extractor import extract_pdf_data
 from risikogebiete_analysis.pdf_analysis.pdf_parser import analyse_pdf
 from risikogebiete_analysis.document_download.page_scraper \
     import get_page_content
@@ -14,7 +20,7 @@ from risikogebiete_analysis.document_download.file_downloader \
     import manage_downloads
 
 
-def get_reports():
+async def get_reports():
     root_url = 'https://www.rki.de'
     url = (
         'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_'
@@ -22,20 +28,74 @@ def get_reports():
     )
     results = get_page_content(url)
     missing_files = get_missing_files(results)
-    asyncio.run(manage_downloads(missing_files, root_url))
+    await manage_downloads(missing_files, root_url)
+
+
+def filename_to_datetime(filename):
+    def either(iterable, key, default):
+        try:
+            return iterable[key]
+        except IndexError:
+            return default
+
+    digits = re.findall(r'\d+', filename)
+    params = (digits[0][4:], digits[0][2:4], digits[0][:2],
+              either(digits, 1, 0), either(digits, 2, 0))
+    params = (int(el) for el in params)
+    return datetime(*params).isoformat()
+
+
+async def analyse_report(path):
+    analysis = analyse_pdf(extract_pdf_data(path))
+    timestamp = filename_to_datetime(path.split('/')[-1])
+    print(f'analysed report: {path.split("/")[-1]}')
+    return timestamp, analysis
+
+
+async def analyse_all_reports(directory):
+    tasks = [analyse_report(f'{directory}/{file}')
+             for file in os.listdir(directory)]
+    return await asyncio.gather(*tasks)
+
+
+async def write_to_file(data, filename):
+    data = sorted(data, key=lambda el: el[0])
+    async with aiofiles.open(filename, mode='w') as file:
+        for item in data:
+            await file.write(f'{item[0]}: {str(item[1])}\n')
+
+
+async def write_to_csv(data, filename):
+    data = sorted(data, key=lambda el: el[0])
+    with open(filename, mode='w') as file:
+        fieldnames = ['timestamp', 'countries']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for line in data:
+            writer.writerow({
+                fieldnames[0]: line[0],
+                fieldnames[1]: ','.join(line[1])
+            })
+
+
+async def extract_data():
+    directory = '../files'
+    analysis = await analyse_all_reports(directory)
+    print(f'analysed {len(analysis)} report{"s" if len(analysis) else ""}')
+    await write_to_csv(analysis, 'data.csv')
 
 
 def main():
-    get_reports()
-    file = '../files/Risikogebiete_27112020.pdf'
-    start_line = 'Folgende Staaten/Regionen gelten aktuell als Risikogebiete:'
-    end_line = 'Gebiete, die zu einem beliebigen Zeitpunkt in den vergangenen'
-    raw_text = simple_extract(file)
-    result = analyse_pdf(raw_text, start_line, end_line)
-
-    for i, (key, val) in enumerate(result.items()):
-        print(f'{val["case"]:<10}{key:<60}{val["details"]}')
-    return 0
+    try:
+        loop = asyncio.get_event_loop()
+        actions = asyncio.gather(get_reports(), extract_data())
+        loop.run_until_complete(actions)
+        loop.close()
+        return 0
+    except KeyboardInterrupt:
+        return 0
+    except RuntimeError:
+        return 1
 
 
 if __name__ == "__main__":
