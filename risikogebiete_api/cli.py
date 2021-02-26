85a1b5beb3b9c81d69991d767f0dd12815ec11e7
19201080 <etienne.j.n@gmail.com>
2021-02-26 13:52:01 +0100

    Add get_path_from_root to files using paths

--- risikogebiete_api/cli.py
+++ risikogebiete_api/cli.py
@@ -22,6 +22,7 @@ from risikogebiete_api.report_export.individual_report \
     import save_individual_reports
 from risikogebiete_api.report_export.complete_report \
     import save_complete_report
+from risikogebiete_api.utils import get_path_from_root
 
 logger = logging.getLogger(__name__)
 LOG_FORMAT = ('%(asctime)-15s [%(levelname)-7s]: '
@@ -40,33 +41,33 @@ async def get_reports():
 
 
 async def analyse_report(path):
-    filename = path.split('/')[-1]
+    filename = os.path.basename(path)
     analysis = analyse_pdf(extract_pdf_data(path), filename)
     analysis = sorted(analysis, key=lambda x: x['name'])
     timestamp = filename.replace('.pdf', '')
-    logger.debug(f'analysed report: {path.split("/")[-1]}')
+    logger.debug(f'analysed report: {filename}')
     return timestamp, analysis
 
 
 async def analyse_all_reports(directory):
-    tasks = [analyse_report(f'{directory}{file}')
+    tasks = [analyse_report(get_path_from_root(file, directory))
              for file in os.listdir(directory)]
     return await asyncio.gather(*tasks)
 
 
 async def extract_data():
-    download_directory = '../files/'
-    report_directory = '../individual_reports/'
-    complete_report_name = '../data'
+    download_directory_path = get_path_from_root('files')
+    report_directory_path = get_path_from_root('individual_reports')
+    complete_report_name = 'data'
 
-    if not os.path.exists(download_directory):
+    if not os.path.exists(download_directory_path):
         return
 
-    analysis = await analyse_all_reports(download_directory)
+    analysis = await analyse_all_reports(download_directory_path)
     logger.info(f'analysed {len(analysis)} '
                 f'report{"s" if len(analysis) else ""}')
     return await asyncio.gather(
-        save_individual_reports(analysis, report_directory),
+        save_individual_reports(analysis, report_directory_path),
         save_complete_report(analysis, complete_report_name))
 
 
--- risikogebiete_api/report_export/complete_report.py
+++ risikogebiete_api/report_export/complete_report.py
@@ -10,7 +10,8 @@ import os
 import aiocsv
 import aiofiles
 
-from risikogebiete_api.utils import parse_data_for_csv, parse_data_for_json
+from risikogebiete_api.utils \
+    import parse_data_for_csv, parse_data_for_json, get_path_from_root
 
 logger = logging.getLogger(__name__)
 
@@ -22,16 +23,16 @@ async def save_complete_report(data, filename):
 
 async def save_complete_csv_report(data, filename):
     analysed_dates = [el[0] for el in data]
-    csv_filename = f'{filename}.csv'
-    if not os.path.exists(csv_filename):
-        logger.info(f'{csv_filename} doesn\'t exist, creating new one')
-        await write_to_csv(data, csv_filename)
+    csv_file_path = get_path_from_root(f'{filename}.csv')
+    if not os.path.exists(csv_file_path):
+        logger.info(f'{csv_file_path} doesn\'t exist, creating new one')
+        await write_to_csv(data, csv_file_path)
     else:
-        async with aiofiles.open(csv_filename, mode='r+') as f:
+        async with aiofiles.open(csv_file_path, mode='r+') as f:
             content = await f.read()
             if not content.strip():
-                logger.debug(f'{csv_filename} is empty, writing from scratch')
-                await write_to_csv(data, csv_filename)
+                logger.debug(f'{csv_file_path} is empty, writing from scratch')
+                await write_to_csv(data, csv_file_path)
             else:
                 fieldnames, *saved_data = [
                     line for line in csv.reader(content.splitlines())
@@ -47,16 +48,17 @@ async def save_complete_csv_report(data, filename):
 
 async def save_complete_json_report(data, filename):
     analysed_dates = [el[0] for el in data]
-    json_filename = f'{filename}.json'
-    if not os.path.exists(json_filename):
-        logger.info(f'{json_filename} doesn\'t exist, creating new one')
-        await write_to_json(data, json_filename)
+    json_file_path = get_path_from_root(f'{filename}.json')
+    if not os.path.exists(json_file_path):
+        logger.info(f'{json_file_path} doesn\'t exist, creating new one')
+        await write_to_json(data, json_file_path)
     else:
-        async with aiofiles.open(json_filename, mode='r+') as f:
+        async with aiofiles.open(json_file_path, mode='r+') as f:
             content = await f.read()
             if not content.strip():
-                logger.debug(f'{json_filename} is empty, writing from scratch')
-                await write_to_json(data, json_filename)
+                logger.debug(f'{json_file_path} is empty, '
+                             f'writing from scratch')
+                await write_to_json(data, json_file_path)
             else:
                 saved_data = {k: v for k, v in json.loads(content).items()
                               if k not in analysed_dates}
@@ -67,9 +69,9 @@ async def save_complete_json_report(data, filename):
                                          sort_keys=True, indent=2))
 
 
-async def write_to_csv(data, filename):
+async def write_to_csv(data, file_path):
     data = sorted(data, key=lambda el: el[0])
-    async with aiofiles.open(filename, mode='w') as file:
+    async with aiofiles.open(file_path, mode='w') as file:
         fieldnames = ['timestamp',
                       '\t'.join(['country', 'alpha2', 'alpha3', 'numeric'])]
         writer = aiocsv.AsyncWriter(file)
@@ -77,8 +79,8 @@ async def write_to_csv(data, filename):
         await writer.writerows(parse_data_for_csv(data))
 
 
-async def write_to_json(data, filename):
+async def write_to_json(data, file_path):
     data = sorted(data, key=lambda el: el[0])
     data = {timestamp: countries for timestamp, countries in data}
-    async with aiofiles.open(filename, mode='w') as file:
+    async with aiofiles.open(file_path, mode='w') as file:
         await file.write(json.dumps(data, sort_keys=True, indent=2))
