6840a10506a029436942258d0f704cb8a5970b93
19201080 <etienne.j.n@gmail.com>
2021-03-01 22:34:44 +0100

    Improve extraction of country by simplifying the process

--- risikogebiete_api/pdf_analysis/pdf_parser.py
+++ risikogebiete_api/pdf_analysis/pdf_parser.py
@@ -16,7 +16,7 @@ from country_list import countries_for_language
 from iso3166 import countries_by_alpha2
 
 from risikogebiete_api.pdf_analysis.constants import \
-    COUNTRY_SEPARATORS, INTRO_LINE, END_LINES, BULLETS, REGION_BULLET
+    COUNTRY_SEPARATORS, INTRO_LINE, END_LINES, BULLETS
 from risikogebiete_api.pdf_analysis.mistyped_countries import \
     parse_mistyped_countries
 
@@ -38,7 +38,7 @@ def separator_in_parenthesis(element, sep_index):
 
 def find_complex_case(element):
     separator = [el for sep in sorted(COUNTRY_SEPARATORS, key=len)[::-1]
-                 for el in re.findall(sep, element)]
+                 for el in re.findall(str(sep), element)]
     if not separator:
         return False
     results = [match.start() for match in re.finditer(separator[0], element)
@@ -46,52 +46,15 @@ def find_complex_case(element):
     return results
 
 
-def remove_region_bullets(element):
-    return '\n'.join(line for line in element.split('\n')
-                     if not line.startswith(REGION_BULLET))
-
-
-def remove_gesamt_mention(element):
-    if element:
-        first_world = element[0].split()[0]
-        if first_world.startswith('gesamt'):
-            return [element[0].replace(f'{first_world} ', '')]
-    return element
-
-
-def no_date_in_parenthesis(element):
-    return re.search(r'(\d+.\s?\w+)', element.replace('\n', '')) is None
-
-
-def flatten_list(list_of_list):
-    return [item for sublist in list_of_list for item in sublist]
-
-
-def analyse_complex_country(element, separators, pattern):
-    complex_matches = [element[separators[idx - 1] if idx else None:sep]
-                       for idx, sep in enumerate(separators)]
-    complex_matches = [element for group in complex_matches
-                       for element in group.split('\n')
-                       if not any(element.startswith(sep)
-                                  for sep in COUNTRY_SEPARATORS)]
-    complex_matches = [[element] if no_date_in_parenthesis(element)
-                       else pattern.findall(element.replace('\n', ''))
-                       for element in complex_matches
-                       if element]
-    return flatten_list(complex_matches)
-
-
 def extract_country(element):
     pattern = re.compile(r'(.+)?\(.*\d+.\s?\w+\)?')
-
-    element = remove_region_bullets(element)
+    element = element.replace('\n', '')
+    if found_country := pattern.findall(element):
+        element = found_country[0].replace('gesamt', '')
     if separators := find_complex_case(element):
-        return analyse_complex_country(element, separators, pattern)
-    simple_case = [el for el in pattern.findall(element.replace('\n', ''))]
-    simple_case = remove_gesamt_mention(simple_case)
-    if simple_case:
-        return simple_case
-    return [element]
+        first_separator = separators[0]
+        return element[:first_separator].strip()
+    return element.strip()
 
 
 def find_bullet_list(text, bullets):
@@ -166,8 +129,6 @@ def translate_countries(countries, filename):
 
 def analyse_pdf(stream, filename):
     bullet_list = extract_bullet_list(stream)
-    country_list = flatten_list(extract_country(country)
-                                for country in bullet_list)
-    country_list = [country.strip() for country in country_list]
+    country_list = [extract_country(country) for country in bullet_list]
     country_list = translate_countries(country_list, filename)
     return country_list
